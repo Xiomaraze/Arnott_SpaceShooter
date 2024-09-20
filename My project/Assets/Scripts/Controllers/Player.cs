4251d3b1c214f45163216c1a346e42f77e1b6571
Xiomaraze <allie.arnott@gmail.com>
2024-09-20 14:34:54 -0400

    Importing Unity Project files from Slate

---
+++ My project/Assets/Scripts/Controllers/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public List<Transform> asteroidTransforms;
+    public Transform enemyTransform;
+    public GameObject bombPrefab;
+    public Transform bombsTransform;
+
+    Vector3 velocity = new Vector3(0.001f, 0f);
+
+    
+
+    void Update()
+    {
+        transform.position += velocity;
+    }
+
+}
