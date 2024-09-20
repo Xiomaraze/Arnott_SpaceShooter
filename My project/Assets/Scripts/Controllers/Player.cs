a4215b5c9cc593b0e2025ebd43c8c1b25c054b0f
Xiomaraze <allie.arnott@gmail.com>
2024-09-20 16:21:16 -0400

    Added Movement and Acceleration

--- My project/Assets/Scripts/Controllers/Player.cs
+++ My project/Assets/Scripts/Controllers/Player.cs
@@ -9,13 +9,60 @@ public class Player : MonoBehaviour
     public GameObject bombPrefab;
     public Transform bombsTransform;
 
-    Vector3 velocity = new Vector3(0.001f, 0f);
+    private Vector3 velocity = Vector3.zero;
+    public float targetSpeed = 3f;
+    public float timeTakenToMaxSpeed = 2f;
+    string speed;
 
-    
+    private float acceleration = 1f;
+
+    private void Start()
+    {
+        acceleration = targetSpeed / timeTakenToMaxSpeed;
+    }
+
+    public void PlayerMovement()
+    {
+        //velocity = Vector3.zero;
+        //if ((Input.GetKey(KeyCode.D)) | (Input.GetKey(KeyCode.RightArrow)))
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            //moving right
+            velocity += Vector3.right * acceleration * Time.deltaTime;
+        }
+        //else if ((Input.GetKey(KeyCode.A)) | (Input.GetKey(KeyCode.LeftArrow)))
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            //moving left
+            velocity += Vector3.left * acceleration * Time.deltaTime;
+        }
+        //if ((Input.GetKey(KeyCode.W)) | (Input.GetKey(KeyCode.UpArrow)))
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            //moving up
+            velocity += Vector3.up * acceleration * Time.deltaTime;
+        }
+        //else if ((Input.GetKey(KeyCode.S)) | (Input.GetKey(KeyCode.DownArrow)))
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            //moving down
+            velocity += Vector3.down * acceleration * Time.deltaTime;
+        }
+        float temp;
+        temp = velocity.magnitude;
+        temp = Mathf.Abs(temp);
+        if (temp > targetSpeed)
+        {
+            velocity = velocity.normalized * targetSpeed;
+        }
+        transform.position += velocity * Time.deltaTime;
+        speed = velocity.magnitude.ToString();
+    }
 
     void Update()
     {
-        transform.position += velocity;
+        PlayerMovement();
+        
     }
 
 }
