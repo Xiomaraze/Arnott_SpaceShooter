aa5273688466cb6e5d0d54994052d666911a3a47
Xiomaraze <allie.arnott@gmail.com>
2024-09-26 19:17:36 -0400

    Added theoretical deceleration and enemy movement.
    Deceleration is not quite working as intended.

--- My project/Assets/Scripts/Controllers/Enemy.cs
+++ My project/Assets/Scripts/Controllers/Enemy.cs
@@ -3,8 +3,22 @@ using System.Collections;
 
 public class Enemy : MonoBehaviour
 {
+    Vector3 velocity;
+    public float speed;
+    public Transform player;
+
+    public void EnemyMovement()
+    {
+        Vector3 direction = player.position - transform.position;
+        direction.Normalize();
+        velocity = direction * speed * Time.deltaTime;
+        transform.position += velocity * Time.deltaTime;
+    }
+
     private void Update()
     {
+        EnemyMovement();
+        Debug.Log(transform.position);
     }
 
 }
--- My project/Assets/Scripts/Controllers/Player.cs
+++ My project/Assets/Scripts/Controllers/Player.cs
@@ -15,6 +15,8 @@ public class Player : MonoBehaviour
     string speed;
 
     private float acceleration = 1f;
+    bool forBack;
+    bool leftRight;
 
     private void Start()
     {
@@ -23,30 +25,47 @@ public class Player : MonoBehaviour
 
     public void PlayerMovement()
     {
+        //Tasks 1A, 1B, 1C
+        //Tasks 1A and 1B were completed in class
+
+
         //velocity = Vector3.zero;
         //if ((Input.GetKey(KeyCode.D)) | (Input.GetKey(KeyCode.RightArrow)))
         if (Input.GetKey(KeyCode.RightArrow))
         {
             //moving right
             velocity += Vector3.right * acceleration * Time.deltaTime;
+            leftRight = true;
         }
         //else if ((Input.GetKey(KeyCode.A)) | (Input.GetKey(KeyCode.LeftArrow)))
         else if (Input.GetKey(KeyCode.LeftArrow))
         {
             //moving left
             velocity += Vector3.left * acceleration * Time.deltaTime;
+            leftRight = true;
+        }
+        else
+        { 
+            if (velocity.magnitude > 0) 
+            leftRight = false;
         }
         //if ((Input.GetKey(KeyCode.W)) | (Input.GetKey(KeyCode.UpArrow)))
         if (Input.GetKey(KeyCode.UpArrow))
         {
             //moving up
             velocity += Vector3.up * acceleration * Time.deltaTime;
+            forBack = true;
         }
         //else if ((Input.GetKey(KeyCode.S)) | (Input.GetKey(KeyCode.DownArrow)))
         else if (Input.GetKey(KeyCode.DownArrow))
         {
             //moving down
             velocity += Vector3.down * acceleration * Time.deltaTime;
+            forBack = true;
+        }
+        else
+        {
+            forBack = false;
         }
         float temp;
         temp = velocity.magnitude;
@@ -55,8 +74,25 @@ public class Player : MonoBehaviour
         {
             velocity = velocity.normalized * targetSpeed;
         }
+        if ((!leftRight) && (!forBack))
+        {
+            Vector3 temp2 = velocity - (velocity.normalized * acceleration * Time.deltaTime);
+            if ((velocity.magnitude > 0) && (temp2.magnitude < 0))
+            {
+                velocity = Vector3.zero;
+            }
+            else if ((velocity.magnitude < 0) && (temp2.magnitude > 0))
+            {
+                velocity = Vector3.zero;
+            }
+            else
+            {
+                velocity = temp2 * Time.deltaTime;
+            }
+            //velocity = velocity - (velocity.normalized * acceleration * Time.deltaTime);
+        }
         transform.position += velocity * Time.deltaTime;
-        speed = velocity.magnitude.ToString();
+        //speed = velocity.magnitude.ToString();
     }
 
     void Update()
